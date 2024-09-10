9e502dacff08ef058a053e8a4e74d1c3857db153
vikarankov <rankovavika@gmail.com>
2024-09-10 14:13:40 +0300

    add exceptions

--- src/main/java/practice/CssSelectors.java
+++ src/main/java/practice/CssSelectors.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class cssSelectors {
+public class CssSelectors {
     WebDriver driver = new ChromeDriver();
 
     @Test
